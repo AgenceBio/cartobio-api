fc53b3c03b99922db4c11d49d591f2556fb8d299
Thomas Parisot <oncletom@users.noreply.github.com>
2020-04-14 16:03:48 +0200

    Test summary route

--- app.test.js
+++ app.test.js
@@ -45,6 +45,36 @@ describe('GET /api/v1/test', () => {
   })
 })
 
+describe('GET /api/v1/summary', () => {
+  test('responds with hardcoded summary', () => {
+    return request(app)
+      .get('/api/v1/summary')
+      .type('json')
+      .set('Authorization', USER_DOC_AUTH_TOKEN)
+      .then((response) => {
+        expect(response.status).toBe(200)
+        expect(response.header['content-type']).toBe('application/json; charset=utf-8')
+        expect(response.body).toHaveProperty('type', 'FeatureCollection')
+        expect(response.body.features).toHaveLength(2)
+        expect(response.body.features[0].properties).toMatchObject({
+          pacage: '026000001',
+          numerobio: 1,
+          nom: 'Nom Opérateur',
+          departement: '26',
+          active: true
+        })
+
+        expect(response.body.features[1].geometry).toMatchObject({
+          type: 'Point',
+          coordinates: [
+            5.242270244026442,
+            44.6261547076767
+          ]
+        })
+      })
+  })
+})
+
 describe('GET /api/v1/parcels', () => {
   test('responds with hardcoded parels', () => {
     return request(app)
