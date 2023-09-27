6fcd2d41da759191b5615ce36ad6c29481ced6d0
Thomas Parisot <thom4parisot@users.noreply.github.com>
2023-09-27 17:41:59 +0200

    fix(features): utilise updateCollectionFeatures() pour la mise à jour de masse, plutôt que deemMergeObjects

--- server.test.js
+++ server.test.js
@@ -2,6 +2,12 @@ const { server: app, close, ready } = require('./server')
 const config = require('./lib/config.js')
 const request = require('supertest')
 const { createSigner } = require('fast-jwt')
+const got = require('got')
+const db = require('./lib/db.js')
+
+const agencebioOperator = require('./lib/providers/__fixtures__/agence-bio-operateur.json')
+const record = require('./lib/providers/__fixtures__/record-with-features.json')
+const patchedRecordExpectation = require('./lib/providers/__fixtures__/record-with-features-patched.json')
 
 const sign = createSigner({ key: config.get('jwtSecret') })
 const USER_DOC_AUTH_TOKEN = sign({ ocId: 0, test: true })
@@ -10,6 +16,16 @@ const USER_DOC_AUTH_HEADER = `Bearer ${USER_DOC_AUTH_TOKEN}`
 // start and stop server
 beforeAll(() => ready())
 afterAll(() => close())
+afterEach(() => jest.clearAllMocks())
+
+jest.mock('got', () => ({
+  get: jest.fn(),
+  post: jest.fn()
+}))
+
+jest.mock('./lib/db.js', () => ({
+  query: jest.fn()
+}))
 
 describe('GET /', () => {
   test('responds with a 404', () => {
@@ -101,6 +117,74 @@ describe('POST /api/v2/convert/shapefile/geojson', () => {
       .attach('archive', 'test/fixtures/telepac-parcelles.zip')
       .then((response) => {
         expect(response.status).toBe(401)
+      })
+  })
+})
+
+describe('PATCH /api/v2/audits/:recordId/parcelles', () => {
+  const fakeAbToken = 'abtoken-bbbb-cccc-dddd'
+  const getMock = jest.mocked(got.get)
+  const postMock = jest.mocked(got.post)
+  const queryMock = jest.mocked(db.query)
+
+  test('it updates only the patched properties of the features', () => {
+    // 1. fetchAuthToken
+    postMock.mockReturnValueOnce({
+      async json () {
+        return { token: fakeAbToken }
+      }
+    })
+
+    // 2. enforceRecord + fetchOperatorById
+    queryMock.mockResolvedValueOnce({ rows: [record] })
+    getMock.mockReturnValueOnce({
+      async json () {
+        return agencebioOperator
+      }
+    })
+
+    // 3. UPDATE
+    queryMock.mockResolvedValueOnce({ rows: [patchedRecordExpectation] })
+
+    return request(app)
+      .patch('/api/v2/audits/054f0d70-c3da-448f-823e-81fcf7c2bf6e/parcelles')
+      .set('Authorization', USER_DOC_AUTH_HEADER)
+      .type('json')
+      .send({
+        type: 'FeatureCollection',
+        features: [
+          {
+            id: 1,
+            properties: {
+              cultures: [
+                {
+                  id: '5910f110-96cb-4557-957c-fb77cae8695f',
+                  CPF: '01.21.12'
+                }
+              ]
+            }
+          },
+          {
+            id: 2,
+            properties: {
+              cultures: [
+                {
+                  id: '5910f110-96cb-4557-957c-fb77cae8695f',
+                  CPF: '01.21.12'
+                }
+              ]
+            }
+          }
+        ]
+      })
+      .then((response) => {
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual(patchedRecordExpectation)
+        expect(queryMock.mock.lastCall).toHaveProperty('1', [
+          '054f0d70-c3da-448f-823e-81fcf7c2bf6e',
+          patchedRecordExpectation.parcelles,
+          null
+        ])
       })
   })
 })
