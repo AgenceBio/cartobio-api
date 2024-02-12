96929141f0bca3765a39f8c0cdde0d8a5af82865
Maud Royer <hello@maudroyer.fr>
2024-02-12 12:42:58 +0100

    fix: ne supprime par la commune quand on la connait
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

---
+++ migrations/20240212113820-fix-commune-trigger.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = async function(db) {
+  await db.runSql(
+    /* sql */`
+    CREATE OR REPLACE FUNCTION update_communes() RETURNS trigger AS $$
+    BEGIN
+        NEW.commune = (SELECT CASE
+          WHEN NEW.commune IS NULL AND ST_IsValid(NEW.geometry)
+          THEN (
+              SELECT code
+              FROM communes
+              WHERE ST_Intersects(
+                  NEW.geometry,
+                  communes.geometry
+              ) LIMIT 1
+          )::text
+          ELSE NEW.commune
+          END
+        );
+        RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;
+  `)
+};
+
+exports.down = function(db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
