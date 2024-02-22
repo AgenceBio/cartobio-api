4df499ee8e9c3ee984c673ac718d9ff616ad5898
Maud Royer <hello@maudroyer.fr>
2024-02-22 16:09:10 +0100

    fix: enum export in types

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -2,5 +2,5 @@ export type {HistoryEntry} from './outputs/types/history.d.ts';
 export type {NormalizedRecord, NormalizedRecordSummary} from './outputs/types/record.d.ts';
 export type {AgenceBioNormalizedOperator, AgenceBioNormalizedOperatorWithRecord} from './outputs/types/operator.d.ts';
 export type {CartoBioFeature, CartoBioFeatureCollection, CartoBioFeatureProperties} from './outputs/types/features.d.ts';
-export type {EtatProduction, CertificationState} from './outputs/record.js';
-export type {EventType} from './outputs/history.js';
+export {EtatProduction, CertificationState} from './outputs/record.js';
+export {EventType} from './outputs/history.js';
