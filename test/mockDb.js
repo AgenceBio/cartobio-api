599299a71727106a3e1c1876f02465d8ab8ddfa6
Maud Royer <hello@maudroyer.fr>
2024-03-21 17:37:03 +0100

    chore: use real database for tests
    Signed-off-by: Maud Royer <hello@maudroyer.fr>

---
+++ test/mockDb.js
@@ -0,0 +1,77 @@
+jest.mock('../lib/db.js', () => {
+  const { Client } = require('pg')
+  const pgTypes = require('pg').types
+  pgTypes.setTypeParser(
+    pgTypes.builtins.DATE,
+    (value) => value === null ? null : new Date(value).toISOString().split('T')[0]
+  )
+  const connectionString = require('../lib/config.js').get('databaseUrl')
+  const testDatabaseName = require('../lib/config.js').get('testDatabaseName')
+
+  const url = new URL(connectionString)
+
+  // every app query will
+  // go through this client
+  const client = new Client({
+    user: url.username,
+    password: url.password,
+    host: url.hostname,
+    port: url.port,
+    database: testDatabaseName
+  })
+
+  // this mock will be return by the pool.connect()
+  const _clientQuery = jest.fn(async (...args) => {
+    if (args[0].startsWith('BEGIN')) {
+      return client.query.bind(client)('SAVEPOINT test')
+    }
+
+    if (args[0].startsWith('ROLLBACK')) {
+      return client.query.bind(client)('ROLLBACK TO SAVEPOINT test')
+    }
+
+    if (args[0].startsWith('COMMIT')) {
+      return client.query.bind(client)('RELEASE SAVEPOINT test')
+    }
+
+    return client.query.bind(client)(...args)
+  })
+
+  const _clientRelease = jest.fn()
+
+  return {
+    // return a mock Pool object which
+    // pass all calls to our testing client
+    query: jest.fn(client.query.bind(client)),
+    connect: jest.fn(async () => ({
+      query: _clientQuery,
+      release: _clientRelease
+    })),
+    // to be used to manage the test client from inside the tests
+    _connect: client.connect.bind(client),
+    _end: client.end.bind(client),
+    _clientQuery: _clientQuery, // so we can test calls
+    _clientRelease: _clientRelease // so we can test calls
+  }
+})
+
+beforeAll(async () => {
+  const client = require('../lib/db.js')
+  await client._connect()
+})
+
+beforeEach(async () => {
+  const client = require('../lib/db.js')
+  await client.query('BEGIN')
+  client.query.mockClear()
+})
+
+afterEach(async () => {
+  const client = require('../lib/db.js')
+  await client.query('ROLLBACK')
+})
+
+afterAll(async () => {
+  const client = require('../lib/db.js')
+  await client._end()
+})
