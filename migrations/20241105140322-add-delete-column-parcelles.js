46f4ee64c567c0f68519df96803985ce6ce79a71
Hugo BERTOUX <hbe@atolcd.com>
2024-11-15 14:01:41 +0100

    feat - Rajout from_parcelles

--- migrations/20241105140322-add-delete-column-parcelles.js
+++ migrations/20241105140322-add-delete-column-parcelles.js
@@ -1,27 +1,32 @@
-'use strict';
+"use strict";
 
 var dbm;
 var type;
 var seed;
 
-
-exports.setup = function(options, seedLink) {
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-exports.up = async function(db) {
-  await db.addColumn('cartobio_parcelles', 'deleted_at', {
-    type: 'datetime',
-    notNull: false
+exports.up = async function (db) {
+  await db.addColumn("cartobio_parcelles", "deleted_at", {
+    type: "datetime",
+    notNull: false,
+  });
+  await db.addColumn("cartobio_parcelles", "from_parcelles", {
+    type: "string",
+    length: 255,
+    notNull: false,
   });
 };
 
-exports.down = async function(db) {
-  await db.removeColumn('cartobio_parcelles', 'deleted_at');
+exports.down = async function (db) {
+  await db.removeColumn("cartobio_parcelles", "deleted_at");
+  await db.removeColumn("cartobio_parcelles", "from_parcelles");
 };
 
 exports._meta = {
-  "version": 1
+  version: 1,
 };
