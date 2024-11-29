7e250be4395f52782c90e4bcd4bb143e487138a3
Hugoobx <hugo.bertoux@icloud.com>
2024-11-29 15:43:53 +0100

    feat: Modification api parcellaire

--- server.test.js
+++ server.test.js
@@ -1333,16 +1333,16 @@ describe('POST /api/v2/certification/parcelles', () => {
       .post('/api/v2/certification/parcelles')
       .set('Authorization', fakeOcToken)
       .send(apiParcellaire)
-    expect(db.query).not.toHaveBeenCalled()
+    expect(db.query).toHaveBeenCalled()
     expect(db.connect).toHaveBeenCalled()
-    expect(db._clientQuery).toHaveBeenCalledWith('ROLLBACK;')
     expect(db._clientRelease).toHaveBeenCalled()
     expect(res.status).toBe(400)
     expect(mockSentry).not.toHaveBeenCalled()
     expect(res.body).toEqual({
-      nbObjetTraites: 6,
-      nbObjetAcceptes: 1,
-      nbObjetRefuses: 5,
+      nbObjetTraites: 8,
+      nbObjetAcceptes: 2,
+      nbObjetRefuses: 6,
+      listeWarning: [],
       listeProblemes: [
         // in case of error, check `createOrUpdateOperatorRecord()` SQL arity
         '[#2] champ dateAudit incorrect',
@@ -1367,7 +1367,7 @@ describe('POST /api/v2/certification/parcelles', () => {
       .set('Authorization', fakeOcToken)
       .send(validApiParcellaire)
 
-    expect(db.query).not.toHaveBeenCalled()
+    expect(db.query).toHaveBeenCalled()
     expect(db.connect).toHaveBeenCalled()
     expect(db._clientQuery).toHaveBeenLastCalledWith('COMMIT;')
     expect(db._clientRelease).toHaveBeenCalled()
