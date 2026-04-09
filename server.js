c503966189ca4cd98d83e45a368264c20ac1ae6e
Hugoobx <hugo.bertoux@icloud.com>
2026-04-09 12:13:01 +0200

    fix: eslint error

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ const LRUCache = require('mnemonist/lru-map-with-delete')
 const { randomUUID } = require('node:crypto')
 const { PassThrough } = require('stream')
 const JSONStream = require('jsonstream-next')
-const { createSigner,createDecoder } = require('fast-jwt')
+const { createSigner, createDecoder } = require('fast-jwt')
 const { fetchOperatorByNumeroBio, getUserProfileById, getUserProfileFromSSOToken, verifyNotificationAuthorization, fetchUserOperators, fetchCustomersByOc } = require('./lib/providers/agence-bio.js')
 const { addRecordFeature, createFeaturesFromOther, patchFeatureCollection, updateAuditRecordState, updateFeature, createOrUpdateOperatorRecord, deleteSingleFeature, getRecords, deleteRecord, getOperatorLastRecord, searchControlBodyRecords, getDepartement, recordSorts, pinOperator, unpinOperator, consultOperator, getDashboardSummary, exportDataOcId, searchForAutocomplete, getImportPAC, hideImport, markFeatureControlled, markFeatureUncontrolled } = require('./lib/providers/cartobio.js')
 const {
