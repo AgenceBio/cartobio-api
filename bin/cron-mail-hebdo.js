7348525a5ef1f36634e60828e429153f2f665800
Hugoobx <hugo.bertoux@icloud.com>
2026-04-20 09:42:04 +0200

    feat: rajout table organisme certificateur

--- bin/cron-mail-hebdo.js
+++ bin/cron-mail-hebdo.js
@@ -1,19 +1,26 @@
 #!/usr/bin/env node
 
 const { sendRapportHebdo } = require('../lib/mailer/utils.js')
+const pool = require('../lib/db')
+
 // TODO : WIP => Comment on gére les différents mails ainsi que les ocIds
 async function sendMail () {
-  const ocId = 1
-  const recipientEmail = 'test@test.fr'
+  const { rows: ocList } = await pool.query(`
+      SELECT id, email
+      FROM organisme_certificateur
+      WHERE active = true
+    `)
 
-  if (!ocId || !recipientEmail) {
-    console.error('OC_ID et RECIPIENT_EMAIL sont requis')
+  if (!ocList) {
+    console.error('Aucun organisme certificateur')
     process.exit(1)
   }
 
   try {
-    await sendRapportHebdo(ocId, recipientEmail)
-    console.log(`Rapport envoyé à ${recipientEmail}`)
+    for (const oc in ocList) {
+      await sendRapportHebdo(oc.id, oc.email)
+      console.log(`Rapport envoyé à ${oc.email}`)
+    }
     process.exit(0)
   } catch (err) {
     console.error('Erreur envoi rapport:', err)
---
+++ migrations/20260420072353-ajout-table-oc.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = async function(db) {
+  await db.runSql(
+  `CREATE TABLE IF NOT EXISTS organisme_certificateur (
+    id SERIAL PRIMARY KEY,
+    label TEXT NOT NULL,
+    email TEXT NOT NULL,
+    active BOOLEAN NOT NULL default FALSE
+  )`)
+};
+
+exports.down = function(db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
