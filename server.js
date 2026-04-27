cb846a5b367e60e9a3a1a01af1345c7dc646a050
Hugoobx <hugo.bertoux@icloud.com>
2026-04-27 13:54:39 +0200

    fix: api pagination deadlock fix

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ const { PassThrough } = require('stream')
 const { createSigner } = require('fast-jwt')
 
 const { fetchOperatorByNumeroBio, getUserProfileById, getUserProfileFromSSOToken, verifyNotificationAuthorization, fetchUserOperators, fetchCustomersByOc } = require('./lib/providers/agence-bio.js')
-const { addRecordFeature, createFeaturesFromOther, patchFeatureCollection, updateAuditRecordState, updateFeature, createOrUpdateOperatorRecord, deleteSingleFeature, getRecords, deleteRecord, getOperatorLastRecord, searchControlBodyRecords, getDepartement, recordSorts, pinOperator, unpinOperator, consultOperator, getDashboardSummary, exportDataOcId, searchForAutocomplete, getImportPAC, hideImport, markFeatureControlled, markFeatureUncontrolled } = require('./lib/providers/cartobio.js')
+const { addRecordFeature, createFeaturesFromOther, patchFeatureCollection, updateAuditRecordState, updateFeature, createOrUpdateOperatorRecord, deleteSingleFeature, getRecords, deleteRecord, getOperatorLastRecord, searchControlBodyRecords, getDepartement, recordSorts, pinOperator, unpinOperator, consultOperator, getDashboardSummary, exportDataOcId, searchForAutocomplete, getImportPAC, hideImport, markFeatureControlled, markFeatureUncontrolled, isHealthy } = require('./lib/providers/cartobio.js')
 const {
   collectFullValidationResults,
   createImportJob,
@@ -979,9 +979,7 @@ app.post('/api/v2/geometry/geometryEquals', mergeSchemas(
 
 app.get('/api/v3/health', async (request, reply) => {
   try {
-    // TODO : SELECT IN BDD
-    // await pool.query('SELECT 1')
-
+    await isHealthy()
     return reply.status(200).send({
       status: 'ok',
       db: 'ok',
