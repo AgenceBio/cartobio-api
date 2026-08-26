27b1164b66e5197f2258405a7d4b3d10aad95dff
Hugoobx <hugo.bertoux@icloud.com>
2026-08-26 13:56:14 +0200

    fix: repassage en js pour les tests

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,9 @@
 /** @type {import('jest').Config} */
 const config = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  globalSetup: "./test/setup.js",
+  setupFilesAfterEnv: ["./test/mockDb.js"],
 
-  globalSetup: './test/setup.js',
-  setupFilesAfterEnv: ['./test/mockDb.js'],
+  testMatch: ["**/*.test.ts", "**/*.test.js"],
+};
 
-  testMatch: ['**/*.test.ts', '**/*.test.js'],
-  clearMocks: true,
-  restoreMocks: true,
-}
-
-module.exports = config
+module.exports = config;
